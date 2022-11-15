30da97b951cdc0fa963a51c5d3f900cac10dfd32
MHN715 <nielsen.177@gmail.com>
2022-11-15 13:44:52 +0100

    working template, needs styling

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import FileUpload from "./components/FileUpload";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h4>File Upload</h4>
+
+      <FileUpload />
+    </div>
+  );
+}
