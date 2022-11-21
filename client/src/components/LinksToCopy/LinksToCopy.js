5324e538d81de1a2042044535b5e572a8b8c2b43
MHN715 <nielsen.177@gmail.com>
2022-11-21 13:13:08 +0100

    all generated links are shown to the user, and copy button works

--- client/src/components/LinksToCopy/LinksToCopy.js
+++ client/src/components/LinksToCopy/LinksToCopy.js
@@ -1,20 +1,43 @@
+import { useState, useContext } from "react";
 import "./LinksToCopy.css";
+import ContextStates from "../../context/ContextStates";
 
-export default function LinksToCopy({ fileLink }) {
+export default function LinksToCopy() {
+  const { fileLinks } = useContext(ContextStates);
+
+  async function copyToClipBoard(e) {
+    const copiedMessage = e.target.nextElementSibling;
+    const currentLink = e.target.attributes[0].value;
+
+    if (!navigator.clipboard) {
+      console.log("Clipboard API not available");
+      return;
+    }
+    try {
+      await navigator.clipboard.writeText(currentLink);
+      copiedMessage.textContent = "Copied to clipboard";
+    } catch (err) {
+      console.error("Failed to copy!", err);
+    }
+  }
+
   return (
     <div className="links-to-copy">
-      <div className="link-wrapper">
-        <p className="link">https://google.com</p>
-        <button className="copy-btn">Copy</button>
-      </div>
-      <div className="lin½-wrapper">
-        <p className="link">https://google.com</p>
-        <button className="copy-btn">Copy</button>
-      </div>
-      <div className="link-wrapper">
-        <p className="link">https://google.com</p>
-        <button className="copy-btn">Copy</button>
-      </div>
+      {fileLinks.map((link, index) => {
+        return (
+          <div className="link-wrapper" key={index}>
+            <p className="link">{link}</p>
+            <button
+              data-msg={link}
+              onClick={copyToClipBoard}
+              className="copy-btn"
+            >
+              Copy
+            </button>
+            <span></span>
+          </div>
+        );
+      })}
     </div>
   );
 }
